4c8703096d5993d0fd4726e8bcd01f80dec475bb
Daniela Krúželová <kruzelova.@mail.muni.cz>
2022-11-06 17:25:42 +0100

    LoadAeon - correction of sync/async selection

--- reactapp/src/LoadAeon.jsx
+++ reactapp/src/LoadAeon.jsx
@@ -9,6 +9,8 @@ export default class LoadAeon extends React.Component {
         value : this.props.value || StateApp.LoadAeon,
         selectedFile: this.props.selectedFile || null,
         isOpen: false,
+        sync: false,
+        async: false,
     };
   //  const [param_arguments, set_param_arguments] = useState();
 
@@ -125,11 +127,11 @@ export default class LoadAeon extends React.Component {
                 </h3>
                 <form>
                 <div class="form-check form-check-inline">
-                  <input type="checkbox" class="cblack" id="async" name="semantics" value="1" onChange={event => this.setState({async : true})}/>
+                  <input type="checkbox" class="cblack" id="async" name="semantics" value="1" onChange={event => this.setState({async : !this.state.async})}/>
                   <label for="async" class="form-check-label">Asynchroneous</label>
                 </div>
                 <div class="form-check form-check-inline">
-                  <input type="checkbox" class="cblack" id="sync" name="semantics" value="2" onChange={event => this.setState({sync : true})}/>
+                  <input type="checkbox" class="cblack" id="sync" name="semantics" value="2" onChange={event => this.setState({sync : !this.state.sync})}/>
                   <label class="form-check-label" for="sync">Synchroneous</label>
                 </div>
                 </form>
