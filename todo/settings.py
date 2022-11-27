3ecbecb54540d33d6e2512060686350d8ef90f9e
Daniela Krúželová <kruzelova.@mail.muni.cz>
2022-11-27 13:52:17 +0100

    STATIC_ROOT correction

--- todo/settings.py
+++ todo/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = "django-insecure-qj@8!nrxij^nsed=c68ts3%fzxn3d98hz6^h=)*s7gf$s8dlze
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -126,13 +126,13 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'reactapp/build/static')
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'reactapp/build/static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
 django_on_heroku.settings(locals())
