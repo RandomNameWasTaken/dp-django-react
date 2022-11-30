2cef4d034687f6786c6bf3460bcdfc31ec8638e9
Daniela Krúželová <kruzelova.@mail.muni.cz>
2022-11-30 15:58:37 +0100

    secret_key setup

--- todo/settings.py
+++ todo/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 import os
 import django_on_heroku
 import dj_database_url
-from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
